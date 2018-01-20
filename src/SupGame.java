5ed654ec42d770dd592af3efa8b8965f66091968
Supawit Supparat <Supawit.sup@ku.th>
2018-01-20 15:03:22 +0700

    fix issue for SupGame

--- src/SupGame.java
+++ src/SupGame.java
@@ -1,35 +1,24 @@
 import java.util.Random; 
 /**
- * Example guessing game.
  * 
- * @author Supawit Supparat
- * @version 2018.01.11
+ * @author SupawitSupparat
+ * @version 2018.01.20
  */
 public class SupGame extends NumberGame{
 	private int count = 0 ;
-	 /** upper bound for secret number */
+	
+	/** upper bound for secret number */
     private int upperBound;
+   
     /** the solution to the game */
     private int secret;
-    
-
-
-
-    /** Initialize a new default game. */
-    public SupGame()
-    {
-		this(100);		
-    }
 
 	public SupGame(int upperbound) {
 		this.upperBound = upperbound;
 		// create the secret number
-		long seed = System.nanoTime(); 
-		Random rand = new Random(seed);
-		// don't just copy this.
+		Random rand = new Random();
 		// random.nextInt(n) returns a random integer between 0 and n-1, inclusive.
-		this.secret = rand.nextInt(upperbound);
-		super.setMessage("I'm thinking of a number between 1 and 100");
+		this.secret = rand.nextInt(upperbound)+1;
 	}
 
     /**
@@ -37,21 +26,14 @@ public class SupGame extends NumberGame{
      * @param number is the user's guess
      * @return true if guess is correct, false otherwise
      */
-	
     public boolean guess(int number) {
-    	getCount();
+    	count++;
     	if (number == secret) {
     		setMessage("Right! The secret number is "+secret);
     		return true;
     	}
-    	if (number < 3*secret/4) {
-    		setMessage("Your answer is WAY too small.");
-    	}
-    	else if (number < secret) {
+    	if (number < secret) {
     		setMessage("Your answer is too small.");
-    	}
-    	else if (number > secret*4/3) {
-    		setMessage("Your answer is WAY too large.");
     	}
     	else {
     		setMessage("Your answer is too large.");
@@ -66,7 +48,7 @@ public class SupGame extends NumberGame{
     
 	/** Get count of user guesses*/
 	public int getCount() {
-		return count++;
+		return count;
 	}
 	
     @Override
